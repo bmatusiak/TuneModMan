5127ea3933c34808bd42197d15f939cce660e395
Bradley Matusiak <bmatusiak@gmail.com>
2025-01-14 05:13:36 -0500

    sass-loader removed warnings

--- webpack.rules.js
+++ webpack.rules.js
@@ -44,7 +44,12 @@ module.exports = [
       // Translates CSS into CommonJS
       'css-loader',
       // Compiles Sass to CSS
-      'sass-loader',
+      {
+        loader: "sass-loader",
+        options: {
+          warnRuleAsWarning: false,
+        },
+      },
     ],
   },
   // Put your webpack loader rules in this array.  This is where you would put
